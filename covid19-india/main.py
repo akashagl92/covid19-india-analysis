d0d284207e2b4f6cbd519984d19a0b4011229b56
akashagl92 <akash.agl92@gmail.com>
2020-05-06 14:05:16 +0530

    Updated the legend
    Interactive legends updated for Case-Growth Rate and Fatality Growth Rate (Statewise) to include West Bengal

--- covid19-india/main.py
+++ covid19-india/main.py
@@ -648,8 +648,8 @@ for name, color in zip(cases_summary['loc'].unique(), itertools.cycle(Dark2_8)):
         (cases_summary[cases_summary['loc'] == name]['totalConfirmed'].pct_change() < 0).any()) else name
     legend_it.append((check_negative_growth(name), [renderer]))
 
-legend1=Legend(items=legend_it[0:16], location=(10,0), click_policy='hide', title="Click on States to Switch ON/OFF", title_text_font_style = "bold")
-legend2=Legend(items=legend_it[17:33], location=(10,0), click_policy='hide', title="Click on States to Switch ON/OFF", title_text_font_style = "bold")
+legend1=Legend(items=legend_it[0:16], location=(10,21), click_policy='hide', title="Click on States to Switch ON/OFF", title_text_font_style = "bold")
+legend2=Legend(items=legend_it[16:33], location=(10,0), click_policy='hide', title="Click on States to Switch ON/OFF", title_text_font_style = "bold")
 
 w.add_layout(legend1,'right')
 w.add_layout(legend2,'right')
@@ -718,8 +718,8 @@ for name, color in zip(cases_summary['loc'].unique(), itertools.cycle(Dark2_8)):
     legend_it.append((check_negative_growth(name), [renderer]))
 
 
-legend1=Legend(items=legend_it[0:16], location=(10,0), click_policy='hide', title="Click on States to Switch ON/OFF", title_text_font_style = "bold")
-legend2=Legend(items=legend_it[17:33], location=(10,0), click_policy='hide', title="Click on States to Switch ON/OFF", title_text_font_style = "bold")
+legend1=Legend(items=legend_it[0:16], location=(10,21), click_policy='hide', title="Click on States to Switch ON/OFF", title_text_font_style = "bold")
+legend2=Legend(items=legend_it[16:33], location=(10,0), click_policy='hide', title="Click on States to Switch ON/OFF", title_text_font_style = "bold")
 
 x.add_layout(legend1,'right')
 x.add_layout(legend2,'right')
